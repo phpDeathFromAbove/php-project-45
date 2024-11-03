d6b126a78627a2cac93a1b86f4c3c241aa93d921
ALex <veter_liliputov@citydom.ru>
2024-11-03 16:42:19 +0700

    r

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/phpDeathFromAbove/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/phpDeathFromAbove/php-project-45/actions)
+<a href="https://codeclimate.com/github/phpDeathFromAbove/php-project-45/maintainability"><img src="https://api.codeclimate.com/v1/badges/835afc292698da2eb94f/maintainability" /></a>
--- src/Cli.php
+++ src/Cli.php
@@ -1,20 +1,15 @@
-<?php 
 
+<?php
 
 require_once __DIR__ . '../../vendor/autoload.php';
 
-#namespace Php\Project\Cli;
 
 use cli\line;
 use cli\prompt;
 
 function name()
-
 {
-	cli\line('Welcome to the Brain Game!');
-	$name = cli\prompt('May I have your name?');
-	cli\line("Hello, %s!", $name);
+    cli\line('Welcome to the Brain Game!');
+    $name = cli\prompt('May I have your name?');
+    cli\line("Hello, %s!", $name);
 }
-
-
-?>
