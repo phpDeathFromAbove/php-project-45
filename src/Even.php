f4620938134ec3c24c4f9c63384324c457e7b04e
ALex <veter_liliputov@citydom.ru>
2024-11-03 20:44:19 +0700

    Update 5 step

--- src/Even.php
+++ src/Even.php
@@ -13,12 +13,12 @@ function checkEven()
     cli\line('Welcome to the Brain Game!');
     $name = cli\prompt('May I have your name?');
     cli\line("Hello, %s!", $name);
-    cli\line('Answer \"yes\" if the number is even, otherwise answer \"no\".');
+    cli\line('Answer "yes" if the number is even, otherwise answer "no".');
     for ($i = 1; $i<=3; $i++)
     {   
         $numb = random_int(1,40);
         cli\line('Question: ' . $numb);
-        $ans = cli\prompt('Your answer: ');
+        $ans = cli\prompt('Your answer ');
         if (($numb % 2 === 0 && $ans === "yes")||($numb % 2 !== 0 && $ans === "no"))
         {
             cli\line('Correct!');
