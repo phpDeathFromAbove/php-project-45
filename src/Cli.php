aba4c95a17d40a809797594c5f75eb454559a9d4
ALex <veter_liliputov@citydom.ru>
2024-11-04 16:14:56 +0700

    Error

--- src/Cli.php
+++ src/Cli.php
@@ -9,7 +9,7 @@ use cli\prompt;
 
 function name()
 {
-    cli\line('Welcome to the Brain Games!');
+    cli\line('Welcome to the Brain Game!');
     $name = cli\prompt('May I have your name?');
     cli\line("Hello, %s!", $name);
 }
